4a2690a472b7cea2b2a1800217346eb59b13a8f7
Noah Manger <noah.manger@gmail.com>
2016-03-10 20:25:45 -0800

    New video section

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-    $('.js-video-background').each(function(){
-      new VideoBackground($(this));
-    })
+  $('.js-video-background').each(function(){
+    new VideoBackground($(this));
+  });
 
-    new Tabs('.js-cycle');
-    new Tabs
-
+  $('.js-tabs').each(function(){
+    new Tabs($(this));
+  });
 });
 
 var BREAKPOINTS = {
@@ -51,35 +51,26 @@ VideoBackground.prototype.createVideo = function() {
   this.$body.append(video)
 };
 
-var Tabs = function(selector) {
-  this.$body = $(selector);
+var Tabs = function($selector) {
+  this.$body = $selector;
   this.currentTab = 1;
-  this.$body.on('click', '.cycle__button', this.handleClick.bind(this));
+  this.$body.on('click', '[data-controls-tab]', this.handleClick.bind(this));
 
 };
 
 Tabs.prototype.handleClick = function(e) {
-  var tabNumber = $(e.target).data('tab');
-
-  this.hideTab(this.currentTab);
-  this.showTab(tabNumber);
+  var tabNumber = $(e.target).data('controls-tab');
+  this.hide(this.currentTab);
+  this.show(tabNumber);
 };
 
-Tabs.prototype.showTab = function(tabNumber) {
-  var $button = $('.cycle__button[data-tab="' + tabNumber +'"]');
-  var $stage = $('#stage-' + tabNumber);
-  var $content = $('#content-stage-' + tabNumber);
-  $stage.attr('aria-hidden', false);
-  $content.attr('aria-hidden', false);
-  $button.addClass('active');
+Tabs.prototype.show = function(tabNumber) {
+  this.$body.find('[data-controls-tab="' + tabNumber +'"]').addClass('active');
+  this.$body.find('[data-tab="' + tabNumber + '"]').attr('aria-hidden', false);
   this.currentTab = tabNumber;
 };
 
-Tabs.prototype.hideTab = function(tabNumber) {
-  var $button = $('.cycle__button[data-tab="' + tabNumber +'"]');
-  var $stage = $('#stage-' + tabNumber);
-  var $content = $('#content-stage-' + tabNumber);
-  $stage.attr('aria-hidden', true);
-  $content.attr('aria-hidden', true);
-  $button.removeClass('active');
+Tabs.prototype.hide = function(tabNumber) {
+  this.$body.find('[data-controls-tab="' + tabNumber +'"]').removeClass('active');
+  this.$body.find('[data-tab="' + tabNumber + '"]').attr('aria-hidden', true);
 };
